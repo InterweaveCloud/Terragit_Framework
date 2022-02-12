c7473022ff14262dae4a986819e7f66c218c35b0
NetcraftAutomation <faizanrazasuser@gmail.com>
2022-02-12 02:20:03 +0000

    added third s3 bucket

--- ex2-cicd-feature-branch/main.tf
+++ ex2-cicd-feature-branch/main.tf
@@ -61,3 +61,20 @@ resource "aws_s3_bucket" "TestBucket2" {
   }
 
 }
+
+
+resource "aws_s3_bucket" "TestBucket3" {
+  bucket = "my-tf-test-bucket-uyhgyhgjhghuygugjhfgjhf3"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+}
+
