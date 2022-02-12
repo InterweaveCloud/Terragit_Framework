13d3f459a600f9acc080155685a7b8fe83aa119b
NetcraftAutomation <faizanrazasuser@gmail.com>
2022-02-12 02:32:22 +0000

    removed third s3 bucket

--- ex2-cicd-feature-branch/main.tf
+++ ex2-cicd-feature-branch/main.tf
@@ -61,20 +61,3 @@ resource "aws_s3_bucket" "TestBucket2" {
   }
 
 }
-
-
-resource "aws_s3_bucket" "TestBucket3" {
-  bucket = "my-tf-test-bucket-uyhgyhgjhghuygugjhfgjhf3"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-
-  versioning {
-    enabled = true
-  }
-
-}
-
