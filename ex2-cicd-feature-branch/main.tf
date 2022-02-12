04dc4f59e8fa6a2a1dad1318b45be3448fd5df61
NetcraftAutomation <faizanrazasuser@gmail.com>
2022-02-12 02:09:43 +0000

    removed second s3 bucket and additional config

--- ex2-cicd-feature-branch/main.tf
+++ ex2-cicd-feature-branch/main.tf
@@ -2,55 +2,6 @@ resource "aws_s3_bucket" "TestBucket" {
   bucket = "my-tf-test-bucket-uyhgyhgjhghuygugjhfgjhf"
   acl    = "private"
 
-  # Add Server Side Encryption
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.mykey.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    prefix  = "config/"
-    enabled = true
-
-    noncurrent_version_transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
-    }
-
-    noncurrent_version_transition {
-      days          = 60
-      storage_class = "GLACIER"
-    }
-
-    noncurrent_version_expiration {
-      days = 90
-    }
-  }
-}
-
-# Create a KMS key for the S3 bucket
-resource "aws_kms_key" "mykey" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 10
-}
-
-resource "aws_s3_bucket" "TestBucket2" {
-  bucket = "my-tf-test-bucket-uyhgyhgjhghuygugjhfgjhf2"
-  acl    = "private"
-
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
@@ -61,3 +12,5 @@ resource "aws_s3_bucket" "TestBucket2" {
   }
 
 }
+
+
