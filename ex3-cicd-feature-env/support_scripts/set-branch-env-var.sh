97312222ce4ab67e8cd06689ee14e71ee9235b1c
NetcraftAutomation <faizanrazasuser@gmail.com>
2022-02-13 00:50:12 +0000

    replaced echo with export commands

--- ex3-cicd-feature-env/support_scripts/set-branch-env-var.sh
+++ ex3-cicd-feature-env/support_scripts/set-branch-env-var.sh
@@ -2,8 +2,9 @@
 export test352=test352
 echo "test352 is ${test352} in subshell"
 
+# If base ref is empty, it is assumed that the branch is contained withtin the GITHUB_REF
 if [ -z "${GITHUB_BASE_REF}" ]; then
-  echo "GITHUB_BASE_REF is empty"
+  export TF_VAR_branch=${GITHUB_REF##*/}
 else
-  echo "GITHUB_BASE_REF is not empty"
+  export TF_VAR_branch=${GITHUB_BASE_REF}
 fi
