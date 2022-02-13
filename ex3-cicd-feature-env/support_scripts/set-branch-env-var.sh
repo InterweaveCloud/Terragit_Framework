1edecb6d82113a21bf93eb9539b83ef6e8e350f2
NetcraftAutomation <faizanrazasuser@gmail.com>
2022-02-13 00:44:41 +0000

    Added if statement based on GITHUB_BASE_REF

--- ex3-cicd-feature-env/support_scripts/set-branch-env-var.sh
+++ ex3-cicd-feature-env/support_scripts/set-branch-env-var.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 export test352=test352
 echo "test352 is ${test352} in subshell"
+
+if [ -z "${GITHUB_BASE_REF}" ]; then
+  echo "GITHUB_BASE_REF is empty"
+else
+  echo "GITHUB_BASE_REF is not empty"
+fi
