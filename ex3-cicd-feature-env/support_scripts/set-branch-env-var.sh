9138463c61715167b2d13a280189fae681aaba43
NetcraftAutomation <faizanrazasuser@gmail.com>
2022-02-13 01:54:48 +0000

    changed if logic to check is GITHUB_REF is merge

--- ex3-cicd-feature-env/support_scripts/set-branch-env-var.sh
+++ ex3-cicd-feature-env/support_scripts/set-branch-env-var.sh
@@ -3,7 +3,7 @@ export test352=test352
 echo "test352 is ${test352} in subshell"
 
 # If the github ref is merge, then use the base ref to get the branch
-if [[ "${GITHUB_BASE_REF}" = "merge"]]; then
+if [[ "${GITHUB_BASE_REF}" == "merge"]]; then
   echo "TF_VAR_branch=${GITHUB_BASE_REF}" >> $GITHUB_ENV
 else
   echo "TF_VAR_branch=${GITHUB_REF##*/}" >> $GITHUB_ENV
