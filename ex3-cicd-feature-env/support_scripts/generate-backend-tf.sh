631a9e8f0a20a2736b483a6bdc35134a00f66dab
NetcraftAutomation <faizanrazasuser@gmail.com>
2022-02-15 03:59:36 +0000

    Added another config.s3.beckend file

--- ex3-cicd-feature-env/support_scripts/generate-backend-tf.sh
+++ ex3-cicd-feature-env/support_scripts/generate-backend-tf.sh
@@ -14,3 +14,11 @@ terraform {
   }
 }
 EOF
+
+cat << EOF >> config.s3.tfbackend
+bucket     = "$S3_BACKEND_BUCKET"
+key        = "$TF_VAR_branch/terraform.tfstate"
+region     = "$S3_BACKEND_AWS_REGION"
+encrypt    = $S3_BACKEND_ENCRYPT
+kms_key_id = "$S3_BACKEND_KMS_KEY_ID"
+EOF
