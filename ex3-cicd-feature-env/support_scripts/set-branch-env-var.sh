8200d14ba8d5e5e68110e42b10bb0620b14814db
NetcraftAutomation <faizanrazasuser@gmail.com>
2022-02-16 01:40:21 +0000

    Added echo branch vars for debugging

--- ex3-cicd-feature-env/support_scripts/set-branch-env-var.sh
+++ ex3-cicd-feature-env/support_scripts/set-branch-env-var.sh
@@ -2,6 +2,10 @@
 
 github_ref="${GITHUB_REF##*/}"
 
+echo "Github Base ref is ${GITHUB_BASE_REF}" 
+echo "Github ref is ${github_ref}"" 
+
+
 # If the github ref is merge, then use the base ref to get the branch
 if [ "${github_ref}" = "merge" ]; then
   echo "TF_VAR_branch=${GITHUB_BASE_REF}" >> $GITHUB_ENV
