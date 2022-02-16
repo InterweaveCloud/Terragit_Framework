2be33d6edebc7c57ecdbadef1ae2d3cc62f52edb
NetcraftAutomation <faizanrazasuser@gmail.com>
2022-02-16 01:46:22 +0000

    Added echo branch vars for debugging~

--- ex3-cicd-feature-env/support_scripts/set-branch-env-var.sh
+++ ex3-cicd-feature-env/support_scripts/set-branch-env-var.sh
@@ -4,11 +4,16 @@ github_ref="${GITHUB_REF##*/}"
 
 echo "Github Base ref is ${GITHUB_BASE_REF}" 
 echo "Github ref is ${github_ref}"
+echo "github event name is ${GITHUB_EVENT_NAME}"
+echo "Github event name is ${github.event.ref}"
+
 
 
 # If the github ref is merge, then use the base ref to get the branch
 if [ "${github_ref}" = "merge" ]; then
   echo "TF_VAR_branch=${GITHUB_BASE_REF}" >> $GITHUB_ENV
+elif [ "${GITHUB_EVENT_NAME}" = "delete" ]; then
+  echo "TF_VAR_branch=${github.event.ref}" >> $GITHUB_ENV
 else
   echo "TF_VAR_branch=${GITHUB_REF##*/}" >> $GITHUB_ENV
 fi
