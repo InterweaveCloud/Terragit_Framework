435e8ffde22922bb8b69bbdd2cc260e7ee7bfe1c
NetcraftAutomation <faizanrazasuser@gmail.com>
2022-03-08 19:43:02 +0000

    ex1 provisionally complete

--- ex1-tf_git_incompatibility/main/main.tf
+++ ex1-tf_git_incompatibility/main/main.tf
@@ -1,9 +1,13 @@
+//  Retrieve AWS Account number on which terraform is executing
 data "aws_caller_identity" "current" {}
 
+// Generate a globally unique id to append to the resource bucket name
 resource "random_id" "s3_append" {
   byte_length = 4
 }
 
+// Create a bucket name including the AWS account number and unique ID to ensure it is globally unique
+// (requirement for S3 bucket names)
 locals {
   bucket_name  = "ex1-bucket-${data.aws_caller_identity.current.account_id}-${lower(random_id.s3_append.id)}"
 }
