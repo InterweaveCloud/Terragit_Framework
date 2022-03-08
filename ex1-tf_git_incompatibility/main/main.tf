0297b22682b2578223b80c88d07a5136046d67cb
NetcraftAutomation <faizanrazasuser@gmail.com>
2022-03-08 17:34:55 +0000

    modified s3 bucket to ensure it is globally unique without manual intervention

--- ex1-tf_git_incompatibility/main/main.tf
+++ ex1-tf_git_incompatibility/main/main.tf
@@ -1,11 +1,19 @@
 data "aws_caller_identity" "current" {}
 
+resource "random_id" "s3_append" {
+  byte_length = 4
+}
+
+locals {
+  bucket_name  = "ex1-bucket-${data.aws_caller_identity.current.account_id}-${lower(random_id.s3_append.id)}"
+}
+
 resource "aws_s3_bucket" "TestBucket" {
-  bucket = "my-tf-test-bucket-${data.aws_caller_identity.current.account_id}"
+  bucket = local.bucket_name
   acl    = "private"
 
   tags = {
-    Name        = "My bucket"
+    Name        = local.bucket_name
     Environment = "Dev"
   }
 }
